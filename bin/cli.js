a8b74d4863df06b5bed4cac5c019b1a3a08be6af
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2019-06-18 20:55:33 +0200

    Add historical command
    The cli now has a command named "historical". Can be used to make cities historical from the command line.

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,8 @@ const path = require('path');
 const fs = require('fs');
 const program = require('commander');
 const DBPF = require('../lib/dbpf');
+const FileType = require('../lib/file-types');
+const pkg = require('../package.json');
 
 const Style = {
 	"Chicago": 0x00002000,
@@ -15,11 +17,58 @@ const Style = {
 
 // Main program options.
 program
-	.version('0.1.0');
+	.name('sc4')
+	.version(pkg.version);
+
+program
+	.command('historical [city]')
+	.option('--force', 'Force override of the city')
+	.option('-o --output', 'The output path to store the city if you\'re not force-overriding')
+	.action(async function(city) {
+		
+		let dir = process.cwd();
+		let file = path.resolve(dir, city);
+		let ext = path.extname(file);
+		if (ext !== '.sc4') throw new Error(`${file} is not a SimCity 4 savegame!`);
+
+		// Read in the city.
+		console.log(chalk.cyan('READING'), file);
+		let buff = fs.readFileSync(file);
+		let dbpf = new DBPF(buff);
+
+		// Find the lotfile entry.
+		let entry = dbpf.entries.find(entry => entry.type===FileType.LotFile);
+		let lotFile = entry.read();
+		
+		// Loop all lots & make historical.
+		let i = 0;
+		for (let lot of lotFile) {
+			i++;
+			lot.historical = true;
+		}
+		console.log(chalk.green('OK'), chalk.gray('Marked '+i+' lots as historical'));
+
+		// Save again.
+		let out;
+		if (this.force) {
+			out = file;
+		} else {
+			out = this.output;
+			if (!out) {
+				out = 'HISTORICAL-'+path.basename(file);
+			}
+			let dir = path.dirname(file);
+			out = path.resolve(dir, out);
+		}
+
+		console.log(chalk.cyan('SAVING'), out);
+		await dbpf.save({"file": out});
+
+	});
 
 // Some commands.
 program
-	.command('tileset [dir]')
+	.command('tileset [dir]', 'Add all buildings in the given directory to the given tilesets')
 	.option('-b --block', 'Block all buildings from growing')
 	.option('-C --chicago', 'Set the Chicago tileset for all buildings')
 	.option('-N --ny', 'Set the New York tileset for all buildings')
