e171ceba20a87d190e2dae39ac9819061839483e
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2019-06-28 00:30:23 +0200

    Test medium city tiles

--- test/zone-developer-test.js
+++ test/zone-developer-test.js
@@ -35,7 +35,37 @@ describe('The ZoneDeveloper Subfile', function() {
 
 	});
 
-	it.only('should read large city tiles', function() {
+	it('should read medium city tiles', function() {
+
+		let file = path.resolve(__dirname, 'files/City - Medium.sc4');
+		let dbpf = new Savegame(fs.readFileSync(file));
+
+		let entry = dbpf.getByType(FileType.ZoneDeveloperFile);
+		let dev = entry.read();
+		expect(dev.xSize).to.equal(128);
+		expect(dev.zSize).to.equal(128);
+		expect(dev.cells).to.have.length(128);
+		for (let column of dev.cells) {
+			expect(column).to.have.length(128);
+			for (let cell of column) {
+				if (cell) {
+					
+					// Apparently expect assertions are slow (don't know why), 
+					// so assert manually.
+					if (cell.mem === 0) throw new Error();
+					if (cell.type !== FileType.LotFile) throw new Error('Not a lot!');
+				}
+			}
+		}
+
+		// Now serialize again & check that the CRC still matches.
+		let crc = dev.crc;
+		let buff = dev.toBuffer();
+		expect(buff.readUInt32LE(4)).to.equal(crc);
+
+	});
+
+	it('should read large city tiles', function() {
 
 		let file = path.resolve(__dirname, 'files/city.sc4');
 		let dbpf = new Savegame(fs.readFileSync(file));
