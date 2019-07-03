00baf291daa84df2b29808068f7b49591ee4213a
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2019-07-03 12:11:08 +0200

    Decode COMSerializer file

---
+++ test/com-serializer-file-test.js
@@ -0,0 +1,32 @@
+// # com-serializer-file-test.js
+"use strict";
+const chai = require('chai');
+const expect = chai.expect;
+const path = require('path');
+const fs = require('fs');
+const Savegame = require('../lib/savegame');
+const { FileType } = require('../lib/enums');
+
+describe('The COM serializer file', function() {
+
+	it('should parse & serialize correctly', function() {
+
+		let file = path.resolve(__dirname, 'files/city.sc4');
+		let dbpf = new Savegame(fs.readFileSync(file));
+
+		// Find the com serializer file.
+		let entry = dbpf.getByType(FileType.COMSerializerFile);
+		let com = entry.read();
+
+		// Assertions.
+		expect(com.u1).to.equal(1);
+		expect(com.index).to.be.an.instanceOf(Map);
+
+		// Serialize again, buffer should look the same.
+		let check = entry.decompress();
+		let buff = com.toBuffer();
+		expect(buff.toString('hex')).to.equal(check.toString('hex'));
+
+	});
+
+});
