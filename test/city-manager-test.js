35f8c5c86f1df95341818e7f6bfef2ba555467cd
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2019-07-04 17:03:55 +0200

    Create city manager
    A prototype city manager was created. It's this object that will be responsible of plopping buildings in our city.

---
+++ test/city-manager-test.js
@@ -0,0 +1,27 @@
+// # city-manager-test.js
+"use strict";
+const chai = require('chai');
+const expect = chai.expect;
+const CityManager = require('../lib/city-manager');
+const path = require('path');
+
+describe('A city manager', function() {
+
+	it.only('should open a city', async function() {
+
+		let file = path.resolve(__dirname, 'files/City - RCI.sc4');
+		let city = new CityManager(file);
+
+		await city.loadPlugins();
+
+		let exemplar = city.plugins.find(0x6534284a,0xa8fbd372,0xc000c356).read();
+		let objs = exemplar.lotObjects;
+		let building = objs.find(obj => obj.type === 0x0);
+		let iid = building.values[12];
+
+		let bd = city.plugins.records.find(x => x.type === 0x6534284a && x.instance === iid).read();
+		console.log(bd);
+
+	});
+
+});
