88e7758b5f9bbb3452f71a30ae1b1652cbe29353
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2020-03-11 16:34:38 +0100

    Try out plopping buildings

--- test/city-manager-test.js
+++ test/city-manager-test.js
@@ -2,6 +2,7 @@
 "use strict";
 const { expect } = require('chai');
 const path = require('path');
+const FileIndex = require('../lib/file-index.js');
 const CityManager = require('../lib/city-manager.js');
 
 describe('A city manager', function() {
@@ -28,12 +29,42 @@ describe('A city manager', function() {
 		it('returns an unused memory address', async function() {
 
 			let file = path.resolve(__dirname, 'files/City - RCI.sc4');
-			let city = new CityManager(file);
+			let city = new CityManager();
+			city.load(file);
 
 			expect(city.mem()).to.equal(1);
 			city.memRefs.add(2);
 			expect(city.mem()).to.equal(3);
 			expect(city.mem()).to.equal(4);
+
+		});
+
+	});
+
+	context('#plop()', function() {
+
+		it.only('plops a ploppable lot', async function() {
+
+			this.slow(1000);
+
+			// First of all we need to build up a file index that the city 
+			// manager can use.
+			// let dir = path.join(__dirname, 'files/DarkNight_11KingStreetWest');
+			let dir = path.join(__dirname,'files/DiegoDL-432ParkAvenue-LM-DN');
+			let index = new FileIndex(dir);
+			await index.build();
+
+			// Create the city manager.
+			let game = path.join(__dirname, 'files/City - Plopsaland.sc4');
+			let city = new CityManager({ index });
+			city.load(game);
+
+			// Plop it baby.
+			city.plop({
+				tgi: [0x6534284a, 0xd60100c4, 0x483248bb],
+				x: 5,
+				z: 5,
+			});
 
 		});
 
