115771f6d75cf9143ff53b23ececa77d897edf4c
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-07-04 23:52:26 +0200

    Reserialize pipe occupant file

--- test/pipes-test.js
+++ test/pipes-test.js
@@ -1,19 +1,22 @@
 // # pipes-test.js
 'use strict';
+const { expect } = require('chai');
 const fs = require('fs');
 const DBPF = require('sc4/lib/dbpf.js');
 const { getCityPath } = require('../lib/util.js');
 
 describe('The pipes subfile', function() {
 
-	it('is parsed correctly', function() {
+	it('is parsed correctly & serialized correctly', function() {
 
 		let buffer = fs.readFileSync(getCityPath('Pipes', 'Experiments'));
 		let dbpf = new DBPF(buffer);
 
 		let entry = dbpf.find(0x49c05b9f);
+		let raw = entry.decompress();
 		let pipes = entry.read();
-		console.log(pipes);
+		let out = pipes.toBuffer();
+		expect(Buffer.compare(out, raw)).to.equal(0);
 
 	});
 
