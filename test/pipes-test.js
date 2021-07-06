6a7f09419117b7403032a2eb24177147f13bcd16
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-07-07 00:52:30 +0200

    Layout ideal grid

--- test/pipes-test.js
+++ test/pipes-test.js
@@ -49,106 +49,186 @@ describe('The pipes subfile', function() {
 		pipes.length = 0;
 		sim.clear();
 
-		let a = 3;
+		// Bit flags of what connections are enabled. Based on those sides 
+		// we'll also create a map of *lines* corresponding to those sides.
+		const WEST  = 0b0001;
+		const NORTH = 0b0010;
+		const EAST  = 0b0100;
+		const SOUTH = 0b1000;
+		const SIDE_MAP = new Map([
+			[WEST, [0, 1, 0, 0]],
+			[NORTH, [0, 0, 1, 0]],
+			[EAST, [1, 0, 1, 1]],
+			[SOUTH, [1, 1, 0, 1]],
+		]);
+
+		// The function we use for creating a pipe on tile (i, j) with the 
+		// connections given as the byte as it appears in the plumbing 
+		// simulator. Note that this is not sufficient for diagonal tiles, but 
+		// we don't use them anyway, so all is good on that front.
+		const h = 270;
 		let mem = 10;
-		let h = 270;
-		for (let j = 6; j < 64; j += 13) {
-			for (let i = a; i < 61; i++) {
+		function createTile(i, j, id = 0) {
 
-				// Calculate the metric x and z positions.
-				let x = 16*i;
-				let z = 16*j;
-				let xTract = 0x40 + Math.floor(i/4);
-				let zTract = 0x40 + Math.floor(j/4);
+			// Calculate the metric x and z positions of the ne corner of the 
+			// tile.
+			let x = 16*i;
+			let z = 16*j;
 
-				// Create the pipe tile and position it correctly first.
-				let pipe = new Pipe({
-					mem: mem++,
-					xMinTract: xTract,
-					xMaxTract: xTract,
-					zMinTract: zTract,
-					zMaxTract: zTract,
-					x: x+8,
-					y: h-1.4,
-					z: z+8,
-					xMin: x,
-					xMax: x+16,
-					yMin: h-10.2,
-					yMax: h,
-					zMin: z,
-					zMax: z+16,
-					xTile: i,
-					zTile: j,
-				});
-				pipe.yNW = pipe.yNE = pipe.ySW = pipe.ySE = h;
-				pipe.yModel = pipe.y;
+			// Create the pipe tile and position it correctly first.
+			let pipe = new Pipe({
+				mem: mem++,
+				x: x+8,
+				y: h-1.4,
+				z: z+8,
+				xMin: x,
+				xMax: x+16,
+				yMin: h-10.2,
+				yMax: h,
+				zMin: z,
+				zMax: z+16,
+				xTile: i,
+				zTile: j,
+			});
+			pipe.xMinTract = pipe.xMaxTract = 0x40 + Math.floor(i/4);
+			pipe.zMinTract = pipe.zMaxTract = 0x40 + Math.floor(j/4);
+			pipe.yNW = pipe.yNE = pipe.ySW = pipe.ySE = h;
+			pipe.yModel = pipe.y;
 
-				// Set the bottom vertices & bottom texture.
+			// Set the bottom vertices & bottom texture.
+			for (let i = 0; i < 2; i++) {
+				for (let j = 0; j < 2; j++) {
+					let index = 2*i+j;
+					let v = pipe.vertices[index];
+					v.x = x+16*i;
+					v.y = h-10.2;
+					v.z = z+16*(i !== j);
+					v.u = i;
+					v.v = +(i !== j);
+				}
+			}
+			pipe.sideTextures.bottom = pipe.vertices.map(vertex => {
+				let fresh = Object.assign(new Vertex(), vertex);
+				fresh.color = new Color(0xff, 0xff, 0xff, 0x80);
+				return fresh;
+			});
+
+			// Find out the connections for this tile based on the id that was 
+			// specified.
+			const west = !!(id & WEST);
+			const north = !!(id & NORTH);
+			const east = !!(id & EAST);
+			const south = !!(id & SOUTH);
+
+			// Find the sides where there are no connections and hence the 
+			// lines that we need to draw.
+			let lines = [];
+			for (let [side, [di, dj, dii, djj]] of SIDE_MAP.entries()) {
+				if (!(id & side)) {
+					lines.push([
+						[i+di, j+dj],
+						[i+dii, j+djj],
+					]);
+				}
+			}
+
+			// Create the sides of the hole now based on the lines we've 
+			// selected.
+			for (let line of lines) {
 				for (let i = 0; i < 2; i++) {
 					for (let j = 0; j < 2; j++) {
-						let index = 2*i+j;
-						let v = pipe.vertices[index];
-						v.x = x+16*i;
-						v.y = h-10.2;
-						v.z = z+16*(i !== j);
-						v.u = i;
-						v.v = +(i !== j);
+						let vertex = new Vertex();
+						let point = line[i];
+						vertex.x = 16*point[0];
+						vertex.y = h - (i !== j)*10.2;
+						vertex.z = 16*point[1];
+						vertex.u = i;
+						vertex.v = i !== j ? 0.6375007629394531 : 0;
+						vertex.color = new Color(0xff, 0xff, 0xff, 0x80);
+						pipe.sideTextures[0].push(vertex);
 					}
 				}
-				pipe.sideTextures.bottom = pipe.vertices.map(vertex => {
-					let fresh = Object.assign(new Vertex(), vertex);
-					fresh.color = new Color(0xff, 0xff, 0xff, 0x80);
-					return fresh;
-				});
+				pipe.blocks++;
+			}
 
-				// Create the sides of the hole now.
-				let lines = [
-					[[i, j], [i+1, j]],
-					[[i, j+1], [i+1, j+1]],
-				];
-				for (let line of lines) {
-					for (let i = 0; i < 2; i++) {
-						for (let j = 0; j < 2; j++) {
-							let vertex = new Vertex();
-							let point = line[i];
-							vertex.x = 16*point[0];
-							vertex.y = h - (i !== j)*10.2;
-							vertex.z = 16*point[1];
-							vertex.u = i;
-							vertex.v = i !== j ? 0.6375007629394531 : 0;
-							vertex.color = new Color(0xff, 0xff, 0xff, 0x80);
-							pipe.sideTextures[0].push(vertex);
-						}
-					}
-					pipe.blocks++;
-				}
-
-				// Insert the prop model at the correct position.
+			// Count how many connections we have now. This determines what 
+			// model we have to insert as well as how to orient it.
+			let sum = west + north + east + south;
+			if (sum === 1) {
+				pipe.textureId = 0x00000300;
+				pipe.orientation = [south, west, north, east].indexOf(true);
+			} else if (sum === 2) {
 				pipe.textureId = 0x00004b00;
-				pipe.matrix.position = [pipe.x, pipe.y, pipe.z];
+				pipe.orientation = west ? 1 : 0;
+			} else if (sum === 3) {
+				pipe.textureId = 0x00005700;
+				pipe.orientation = [west, north, east, south].indexOf(false);
+			} else if (sum === 4) {
+				pipe.textureId = 0x00020700;
+			}
+
+			// Insert the prop model at the correct position and then rotate 
+			// into place based on the orientation we've set on the tile.
+			pipe.matrix.position = [pipe.x, pipe.y, pipe.z];
+			if (pipe.orientation === 1) {
 				pipe.matrix.ex = [0, 0, 1];
 				pipe.matrix.ez = [-1, 0, 0];
-				pipe.orientation = 1;
-
-				// Set the connections.
-				pipe.eastConnection = 0x02;
-				pipe.westConnection = 0x02;
-
-				// Based on the connections, determine what value to set in 
-				// the plumbing simulator.
-				let base = 0b10000;
-				if (pipe.westConnection) base ^= 0b0001;
-				if (pipe.eastConnection) base ^= 0b0100;
-				if (pipe.northConnection) base ^= 0b0010;
-				if (pipe.southConnection) base ^= 0b1000;
-				sim.cells[j][i] = base;
-
-				// At last insert the pipe.
-				pipes.push(pipe);
-				sim.pipes.push(new Pointer(pipe));
-
+			} else if (pipe.orientation) {
+				pipe.matrix.ex = [-1, 0, 0];
+				pipe.matrix.ez = [0, 0, -1];
+			} else if (pipe.orientation === 3) {
+				pipe.matrix.ex = [0, 0, -1];
+				pipe.matrix.ez = [1, 0, 0];
 			}
+
+			// Manually set our connection values as well.
+			pipe.westConnection = west ? 0x02 : 0;
+			pipe.northConnection = north ? 0x02 : 0;
+			pipe.eastConnection = east ? 0x02 : 0;
+			pipe.southConnection = south ? 0x02 : 0;
+			return pipe;
+
 		}
+
+		// Determine the rows where we have to draw the pipes.
+		const size = 64;
+		let rows = [];
+		for (let j = 7; j < size; j += 13) {
+			rows.push(j);
+		}
+
+		// Draw the horizontal lines now.
+		const vertical = 5;
+		rows.forEach(j => {
+			const a = 3;
+			const b = size-a;
+			for (let i = a; i <= b; i++) {
+				if (i === vertical) continue;
+				let id = i === a ? 20 : (i === b ? 17 : 21);
+				sim.cells[j][i] = id;
+				let pipe = createTile(i, j, id);
+				pipes.push(pipe);
+			}
+		});
+
+		// Draw the vertical line.
+		let [a] = rows;
+		let b = rows[rows.length-1];
+		for (let j = a; j <= b; j++) {
+			let id = 0b10000;
+			if (j !== a) id ^= NORTH;
+			if (j !== b) id ^= SOUTH;
+			if (rows.includes(j)) {
+				id ^= EAST;
+				id ^= WEST;
+			}
+			sim.cells[j][vertical] = id;
+			let pipe = createTile(vertical, j, id);
+			pipes.push(pipe);
+		}
+
+		// Put the pipes in the plumbing simulator.
+		pipes.forEach(pipe => sim.pipes.push(new Pointer(pipe)));
 
 		// Now rebuild the item index and store in the com serializer.
 		// console.table(pipes, getKeys(pipes));
