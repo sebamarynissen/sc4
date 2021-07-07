98f2d70f64a165a2be83bf5f8e56bc7b86a87b37
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-07-07 16:14:11 +0200

    Fix terrain map edge queries

--- test/terrain-map-test.js
+++ test/terrain-map-test.js
@@ -28,6 +28,14 @@ describe('The terrain map', function() {
 
 	});
 
+	it('accepts a size in tile', function() {
+
+		let terrain = new TerrainMap(4);
+		expect(terrain).to.have.length(5);
+		terrain.forEach(row => expect(row).to.have.length(5));
+
+	});
+
 	it('serializes row-first', function() {
 
 		let buff = Buffer.alloc(2+ 4*5**2);
@@ -59,6 +67,19 @@ describe('The terrain map', function() {
 		expect(map.query(16+8, 16)).to.equal(0);
 		expect(map.query(16+16, 16)).to.equal(0);
 
+	});
+
+	it('performs a terrain query on the edges', function() {
+		let map = new TerrainMap(1);
+		map.raw.set([0, 1, 3, 2]);
+		expect(map.query(0, 0)).to.equal(0);
+		expect(map.query(8, 0)).to.equal(0.5);
+		expect(map.query(16, 0)).to.equal(1);
+		expect(map.query(16, 8)).to.equal(1.5);
+		expect(map.query(16, 16)).to.equal(2);
+		expect(map.query(8, 16)).to.equal(2.5);
+		expect(map.query(0, 16)).to.equal(3);
+		expect(map.query(0, 8)).to.equal(1.5);
 	});
 
 	it('performs a terrain query on cliffs', function() {
