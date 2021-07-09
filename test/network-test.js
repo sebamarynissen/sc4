f08a2cf0231bf6031f02b42d46243e147ba3f01d
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-07-09 17:16:14 +0200

    Decode network intersections in network index

--- test/network-test.js
+++ test/network-test.js
@@ -20,7 +20,7 @@ describe('A network tile', function() {
 
 	});
 
-	it.only('plays with values', async function() {
+	it('plays with values', async function() {
 
 		let file = getCityPath('Network');
 		// let file = getCityPath('Wayside', 'New Delphina');
@@ -28,11 +28,29 @@ describe('A network tile', function() {
 		// let file = getCityPath('New Sebastia', 'New Delphina');
 		let buffer = fs.readFileSync(file);
 		let dbpf = new Savegame(buffer);
-		let index = dbpf.networkIndex;
+
+		let map = global.map = new Map();
 		for (let tile of dbpf.network) {
-			if (tile.mem === 16347672) {
-				console.log(tile.xMin/16, tile.zMin/16);
-			}
+			map.set(tile.mem, tile);
+		}
+		for (let tile of dbpf.prebuiltNetwork) {
+			map.set(tile.mem, tile);
+		}
+
+		let blocks = dbpf.networkIndex.intersections;
+		blocks.sort((a, b) => {
+			let at = map.get(+a.pointer);
+			let bt = map.get(+b.pointer);
+			return at.x - bt.x || at.z - bt.z;
+		});
+		for (let block of dbpf.networkIndex.intersections) {
+			let tile = map.get(+block.pointer);
+			let x = tile.x/16-0.5;
+			let z = tile.z/16-0.5;
+			const chalk = require('chalk');
+			// console.log(chalk.yellow(String(x).padStart(3, ' ')), chalk.yellow(String(z).padStart(3, ' '))+' ', block.buffer.toString('hex').chunk([2, 8, 8, 2, 8, 8, 8, 8, 2, 8, 8, 8, 8, 2, 8, 8, 8, 8, 2]));
+			console.log(x, z);
+			console.table([block.west, block.north, block.east, block.south]);
 		}
 
 	});
