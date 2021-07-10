f6fcce6f902fb51e4c6caf54ff70826fe0162e43
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-07-10 22:34:16 +0200

    Improve proof of concept for generating road tiles

--- test/network-test.js
+++ test/network-test.js
@@ -55,67 +55,139 @@ describe('A network tile', function() {
 
 	});
 
-	it.skip('reads the model network', async function() {
+	it.only('reads the model network', async function() {
 
-		// let file = getCityPath('Network');
-		let file = getCityPath('New Delphina', 'New Delphina');
+		let file = getCityPath('Network');
+		// let file = getCityPath('New Delphina', 'New Delphina');
 		let buffer = fs.readFileSync(file);
 		let dbpf = new Savegame(buffer);
 		let { network } = dbpf;
 		network.sort((a, b) => a.z - b.z);
-		console.log(network[0].vertices);
 		console.table(network);
-		console.log(dbpf.networkIndex);
+		console.table(network[2].vertices);
 
 	});
 
-	it.only('draws a road', async function() {
+	it.only('draws roads', async function() {
 
 		let source = getTestFile('City - Road Construction.sc4');
 		let out = getCityPath('Road Construction');
 		let buffer = fs.readFileSync(source);
 		let dbpf = new Savegame(buffer);
 
-		let { network, networkIndex, itemIndex } = dbpf;
-		let i = 10;
-		let j = 5;
-		let x = 16*i;
-		let z = 16*j;
-		let tile = new Network();
-		tile.mem = 0x12345678;
-		tile.xMinTract = tile.xMaxTract = 0x40 + Math.floor(x/64);
-		tile.zMinTract = tile.zMaxTract = 0x40 + Math.floor(z/64);
-		tile.x = x+8;
-		tile.y = 270;
-		tile.z = z+8;
-		let color = new Color(0xde, 0xdb, 0xdd, 0xff);
-		Object.assign(tile.vertices[0], { x: x, y: 270, z: z, u: 0, v: 0, color });
-		Object.assign(tile.vertices[1], { x: x, y: 270, z: z+16, u: 0, v: 1, color });
-		Object.assign(tile.vertices[2], { x: x+16, y: 270, z: z+16, u: 1, v: 1, color });
-		Object.assign(tile.vertices[3], { x: x+16, y: 270, z: z, u: 1, v: 0, color });
-		tile.textureId = 0x00000100;
-		tile.xMin = x;
-		tile.xMax = x+16;
-		tile.yMin = tile.yMax = 270;
-		tile.zMin = z;
-		tile.zMax = z+16;
-		tile.unknown[6][1] = 0x80;
-		network.push(tile);
-
-		let indexTile = networkIndex.tile();
-		indexTile.nr = 64*(tile.z/16-0.5) + tile.x/16 -0.5;
-		indexTile.pointer = new Pointer(network[0]);
-
-		console.log(networkIndex.tiles);
+		// Properly clear the network and the network index.
+		let { network, networkIndex, regionView: info } = dbpf;
+		network.length = 0;
 		networkIndex.tiles = [];
-		networkIndex.tiles.push(indexTile);
 
-		console.log(networkIndex.tiles);
+		const textures = createTable([
+			[0x00000100, 0b0000],
+			[0x00000300, 0b1000, 0b0100, 0b0010, 0b0001],
+			[0x00004b00, 0b1010, 0b0101],
+			[0x00000f00, 0b1100, 0b0110, 0b0011, 0b1001],
+			[0x00020700, 0b1111],
+			[0x00005700, 0b1110, 0b1101, 0b1011, 0b0111],
+		]);
+		const orientations = createTable([
+			[3, 0b1010],
+			[0, 0b0101],
+		]);
 
-		// Rebuild the item index from it and update the com seralizer.
-		itemIndex.rebuild(network);
+		// Helper function for creating a table that maps identifiers to other 
+		// stuff (like textures or orientations).
+		function createTable(arr) {
+			return arr.reduce((map, [value, ...ids]) => {
+				ids.forEach(id => map[id] = value);
+				return map;
+			}, Array(0b10000));
+		}
+
+		// The function that will create a road tile based on the id that 
+		// indicates what connections need to be allowed. Note that this 
+		// obviously doesn't support diagonals and FAR. That's definitely ok 
+		// for now.
+		let mem = 10;
+		function createTile(i, j, id = 0b0000) {
+
+			let x = 16*i;
+			let z = 16*j;
+			let y = 270;
+			let tile = new Network({
+				mem: mem++,
+				x: x+8,
+				y,
+				z: z+8,
+				xMin: x,
+				xMax: x+16,
+				yMin: y,
+				yMax: y,
+				zMin: z,
+				zMax: z+16,
+			});
+
+			// Properly derive the tracts.
+			tile.xMinTract = tile.xMaxTract = 0x40 + Math.floor(tile.x/64);
+			tile.zMinTract = tile.zMaxTract = 0x40 + Math.floor(tile.z/64);
+
+			// Set the texture itself and its orientation.
+			tile.textureId = textures[id];
+			tile.orientation = orientations[id] || 0;
+
+			// Ensure the connections are correctly set.
+			tile.westConnection = (id & 0b1000) ? 0x02 : 0x00;
+			tile.northConnection = (id & 0b0100) ? 0x02 : 0x00;
+			tile.eastConnection = (id & 0b0010) ? 0x02 : 0x00;
+			tile.southConnection = (id & 0b0001) ? 0x02 : 0x00;
+
+			// Set the texture vertices.
+			let color = new Color(0xde, 0xdb, 0xdd, 0xff);
+			Object.assign(tile.vertices[0], { x: x, y, z: z, u: 0, v: 0, color });
+			Object.assign(tile.vertices[1], { x: x, y, z: z+16, u: 0, v: 1, color });
+			Object.assign(tile.vertices[2], { x: x+16, y, z: z+16, u: 1, v: 1, color });
+			Object.assign(tile.vertices[3], { x: x+16, y, z: z, u: 1, v: 0, color });
+
+			// Create a network index tile for it.
+			let indexTile = networkIndex.tile();
+			indexTile.nr = 64*info.xSize*j + i;
+			indexTile.pointer = new Pointer(tile);
+			networkIndex.tiles.push(indexTile);
+
+			// Insert the tile in the network subfile and network index.
+			network.push(tile);
+
+		}
+
+		// The function responsible for actually drawing the street map.
+		function draw(map) {
+			for (let i = 0; i < map.length; i++) {
+				let col = map[i];
+				for (let j = 0; j < col.length; j++) {
+					let id = col[j];
+					if (id) {
+						createTile(i, j, id);
+					}
+				}
+			}
+		}
+
+		// Construct the streetmap.
+		let streetmap = Array(64).fill().map(() => new Uint8Array(64).fill(0));
+		for (let i = 2; i <= 8; i++) {
+			streetmap[i][5] ^= 0b1010;
+		}
+		for (let i = 2; i <= 8; i++) {
+			streetmap[5][i] ^= 0b0101;
+		}
+
+		// And draw it.
+		draw(streetmap);
+
+		// Sort the networkIndex.
+		networkIndex.tiles.sort((a, b) => a.nr - b.nr);
+		dbpf.itemIndex.rebuild(network);
 		dbpf.COMSerializerFile.update(network);
 
+		// Save at last.
 		await dbpf.save(out);
 
 	});
