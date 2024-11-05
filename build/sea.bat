777c9c186f3f394aa0f21caa1d3e66303a280bc5
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-05 22:27:26 +0100

    Properly include version in sea

--- build/sea.bat
+++ build/sea.bat
@@ -1,4 +1,5 @@
 call npm run build:bundle
 node --experimental-sea-config build/sea-config.json
+node -e "require('fs').writeFileSync('dist/version.txt', require('./package.json').version)"
 node -e "require('fs').copyFileSync(process.execPath, 'dist/sc4.exe')"
-call npm run build:postject -- --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
+call npm run build:postject
