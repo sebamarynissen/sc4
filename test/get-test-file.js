6ff8299138b49946d498c098c1956227e6dd2cf3
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-05 23:00:22 +0100

    Migrate core to esm

--- test/get-test-file.js
+++ test/get-test-file.js
@@ -1,13 +1,13 @@
 // # get-test-file.js
-const path = require('node:path');
+import path from 'node:path';
 
 // # getTestFile(file)
 // Helper function that easily gets the path of a given test file in the test 
 // folder.
-module.exports = function getTestFile(file) {
+export default function getTestFile(file) {
 	return path.resolve(
-		__dirname,
+		import.meta.dirname,
 		'./files',
 		file,
 	);
-};
+}
