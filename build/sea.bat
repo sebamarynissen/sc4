4351e0c64e16fc5af0be0aa242f426709b92f74a
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-08 17:44:35 +0100

    Fix build order for version string

--- build/sea.bat
+++ build/sea.bat
@@ -1,5 +1,5 @@
+node -e "require('fs').writeFileSync('dist/version.txt', require('./package.json').version)"
 call npm run build:bundle
 node --experimental-sea-config build/sea-config.json
-node -e "require('fs').writeFileSync('dist/version.txt', require('./package.json').version)"
 node -e "require('fs').copyFileSync(process.execPath, 'dist/sc4.exe')"
 call npm run build:postject
