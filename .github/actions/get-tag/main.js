d25555ef42c1a112d68061687fe1a03781e2a084
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-09 22:28:49 +0100

    Detect prerelease labels

--- .github/actions/get-tag/main.js
+++ .github/actions/get-tag/main.js
@@ -7,6 +7,14 @@ if (github.context.eventName === 'pull_request') {
 		core.setOutput('version', 'major');
 	} else if (labels.includes('minor')) {
 		core.setOutput('version', 'minor');
+	} else if (labels.includes('prepatch')) {
+		core.setOutput('version', 'prepatch');
+	} else if (labels.includes('preminor')) {
+		core.setOutput('version', 'preminor');
+	} else if (labels.includes('premajor')) {
+		core.setOutput('version', 'premajor');
+	} else if (labels.includes('prerelease')) {
+		core.setOutput('version', 'prerelease');
 	} else {
 		core.setOutput('version', 'patch');
 	}
