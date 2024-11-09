b0c0d8d8fbe4054d21f8f5f05def5b7b85a556a8
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-09 11:55:24 +0100

    Read package version

--- .github/actions/get-version/main.js
+++ .github/actions/get-version/main.js
@@ -1,3 +1,8 @@
+import { createRequire } from 'node:module';
 import core from '@actions/core';
 
-console.log(process.env.GITHUB_WORKSPACE);
+const require = createRequire(process.env.GITHUB_WORKSPACE);
+const pkg = require('./package.json');
+console.log(pkg.version);
+
+core.setOutput('version', `v${pkg.version}`);
