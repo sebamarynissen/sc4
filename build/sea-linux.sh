182516dd647464b22f03b31c996ca003d0072d4d
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-10 10:03:31 +0100

    Add linux build commands to workflow

--- build/sea-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-node -e "const fs = require('fs');if(!fs.existsSync('dist'))fs.mkdirSync('dist');fs.writeFileSync('dist/version.txt', require('./package.json').version)"
-npm run build:bundle
-node --experimental-sea-config build/sea-config.json
-cp $(command -v node) sc4 
-npx postject sc4 NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
