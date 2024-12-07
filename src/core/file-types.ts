77897f78e5efaddb27d512eb9e8870d612a84407
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-07 23:48:01 +0100

    Create separate enum for savegame file types

--- src/core/file-types.ts
+++ src/core/file-types.ts
@@ -26,7 +26,42 @@
 // KEYCFG TypeID=A2E3D533
 // RUL TypeID=0A5BCF4B
 // Cohort TypeID=05342861
-const TYPES = {
+
+// Savegame-specific files. These files are only found in .sc4 files, which are 
+// also DBPF files.
+export const SavegameFileType = {
+	Lot: 0xC9BD5D4A,
+	Building: 0xa9bd882d,
+	Prop: 0x2977AA47,
+	Flora: 0xa9c05c85,
+	BaseTexture: 0xc97f987c,
+	Occupant: 0xa9bc9ab6,
+	Pedestrian: 0x896e75af,
+	ItemIndex: 0x098F964D,
+	RegionViewFile: 0xCA027EDB,
+	ZoneDeveloper: 0x498f9b01,
+	LotDeveloper: 0xa990bfe0,
+	PropDeveloper: 0x89c48f47,
+	COMSerializer: 0x499b23fe,
+	Network: 0xc9c05c6e,
+	PrebuiltNetwork: 0x49c1a034,
+	NetworkIndex: 0x6a0f82b2,
+	TractDeveloper: 0x2990c142,
+	LineItem: 0xaa313c9f,
+	DepartmentBudget: 0xe990bffc,
+	PipeFile: 0x49c05b9f,
+	PlumbingSimulator: 0x0990c075,
+	ZoneManager: 0x298f9b2d,
+
+	// Terrain map is a bit special because it is also identified by group and 
+	// instance inside a Savegame, but obviously the type id is still needed 
+	// sometimes.
+	TerrainMap: 0xa9dd6ff4,
+
+} as const;
+
+// Zip all known file types together now.
+export const FileType = {
 
 	Exemplar: 0x6534284A,
 	Cohort: 0x05342861,
@@ -44,30 +79,6 @@ const TYPES = {
 	LUA: 0xca63e2a3,
 	EffectDir: 0xea5118b0,
 
-	// Savegame-specific files.
-	LotFile: 0xC9BD5D4A,
-	BuildingFile: 0xa9bd882d,
-	PropFile: 0x2977AA47,
-	FloraFile: 0xa9c05c85,
-	BaseTextureFile: 0xc97f987c,
-	OccupantFile: 0xa9bc9ab6,
-	Pedestrian: 0x896e75af,
-	ItemIndexFile: 0x098F964D,
-	RegionViewFile: 0xCA027EDB,
-	ZoneDeveloperFile: 0x498f9b01,
-	LotDeveloperFile: 0xa990bfe0,
-	PropDeveloperFile: 0x89c48f47,
-	COMSerializerFile: 0x499b23fe,
-	NetworkFile: 0xc9c05c6e,
-	PrebuiltNetwork: 0x49c1a034,
-	NetworkIndex: 0x6a0f82b2,
-	TractDeveloper: 0x2990c142,
-	LineItem: 0xaa313c9f,
-	DepartmentBudget: 0xe990bffc,
-	PipeFile: 0x49c05b9f,
-	PlumbingSimulator: 0x0990c075,
-	ZoneManager: 0x298f9b2d,
-
 	// Sim grids
 	SimGridFloat32: 0x49b9e60a,
 	SimGridUint32: 0x49b9e606,
@@ -76,16 +87,9 @@ const TYPES = {
 	SimGridSint8: 0x49b9e603,
 	SimGridUint8: 0x49b9e602,
 
-	// Terrain map is a bit special because it is also identified by group and 
-	// instance.
-	TerrainMap: 0xa9dd6ff4,
+	...SavegameFileType,
 
 } as const;
-export default TYPES;
 
-// Make them inversely available as well. Note that we risk overwriting 
-// duplicates, but that's ok.
-// for (let key in TYPES) {
-// 	let value = TYPES[key];
-// 	TYPES[value] = key;
-// }
+// Export them all as the default as well.
+export default FileType;
