f4f57e15d89bbf7af991196b714cbf44bd519cbd
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-07 23:34:09 +0100

    Explicitly export file classes

--- src/core/file-classes.ts
+++ src/core/file-classes.ts
@@ -1,28 +1,28 @@
 // # file-classes.ts
-export { Exemplar, Cohort } from './exemplar.js';
-export { default as DIR } from './dir.js';
-export { default as LTEXT } from './ltext.js';
-export { default as Lot } from './lot.js';
-export { default as Building } from './building.js';
-export { default as Prop } from './prop.js';
-export { default as Flora } from './flora.js';
-export { default as TerrainMap } from './terrain-map.js';
-export { default as BaseTexture } from './lot-base-texture.js';
-export { default as Network } from './network.js';
-export { default as PrebuiltNetwork } from './prebuilt-network.js';
-export { default as LineItem } from './line-item.js';
-export { default as DepartmentBudget } from './department-budget.js';
-export { default as Pipe } from './pipe.js';
-export { default as NetworkIndex } from './network-index.js';
-export { default as ItemIndex } from './item-index.js';
-export { default as RegionView } from './region-view.js';
-export { default as ZoneDeveloper } from './zone-developer-file.js';
-export { default as LotDeveloper } from './lot-developer-file.js';
-export { default as COMSerializer } from './com-serializer-file.js';
-export { default as ZoneManager } from './zone-manager.js';
-export { default as TractDeveloper } from './tract-developer.js';
-export { default as PlumbingSimulator } from './plumbing-simulator.js';
-export {
+import { Exemplar, Cohort } from './exemplar.js';
+import DIR from './dir.js';
+import LTEXT from './ltext.js';
+import Lot from './lot.js';
+import Building from './building.js';
+import Prop from './prop.js';
+import Flora from './flora.js';
+import TerrainMap from './terrain-map.js';
+import BaseTexture from './lot-base-texture.js';
+import Network from './network.js';
+import PrebuiltNetwork from './prebuilt-network.js';
+import LineItem from './line-item.js';
+import DepartmentBudget from './department-budget.js';
+import Pipe from './pipe.js';
+import NetworkIndex from './network-index.js';
+import ItemIndex from './item-index.js';
+import RegionView from './region-view.js';
+import ZoneDeveloper from './zone-developer-file.js';
+import LotDeveloper from './lot-developer-file.js';
+import COMSerializer from './com-serializer-file.js';
+import ZoneManager from './zone-manager.js';
+import TractDeveloper from './tract-developer.js';
+import PlumbingSimulator from './plumbing-simulator.js';
+import {
 	SimGridFloat32,
 	SimGridUint32,
 	SimGridSint16,
@@ -30,3 +30,69 @@ export {
 	SimGridSint8,
 	SimGridUint8,
 } from './sim-grid-file.js';
+
+export {
+	Exemplar,
+	Cohort,
+	DIR,
+	LTEXT,
+	Lot,
+	Building,
+	Prop,
+	Flora,
+	TerrainMap,
+	BaseTexture,
+	Network,
+	PrebuiltNetwork,
+	LineItem,
+	DepartmentBudget,
+	Pipe,
+	NetworkIndex,
+	ItemIndex,
+	RegionView,
+	ZoneDeveloper,
+	LotDeveloper,
+	COMSerializer,
+	ZoneManager,
+	TractDeveloper,
+	PlumbingSimulator,
+	SimGridFloat32,
+	SimGridSint8,
+	SimGridUint8,
+	SimGridSint16,
+	SimGridUint16,
+	SimGridUint32,
+};
+
+export default {
+	Exemplar,
+	Cohort,
+	DIR,
+	LTEXT,
+	Lot,
+	Building,
+	Prop,
+	Flora,
+	TerrainMap,
+	BaseTexture,
+	Network,
+	PrebuiltNetwork,
+	LineItem,
+	DepartmentBudget,
+	Pipe,
+	NetworkIndex,
+	ItemIndex,
+	RegionView,
+	ZoneDeveloper,
+	LotDeveloper,
+	COMSerializer,
+	ZoneManager,
+	TractDeveloper,
+	PlumbingSimulator,
+	SimGridFloat32,
+	SimGridSint8,
+	SimGridUint8,
+	SimGridSint16,
+	SimGridUint16,
+	SimGridUint32,
+} as const;
