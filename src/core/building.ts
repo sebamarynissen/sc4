d48628cbe04ebf1d290fb619963cb4e13286167b
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-08 15:32:02 +0100

    Migrate building file to TypeScript

--- src/core/building.ts
+++ src/core/building.ts
@@ -2,50 +2,53 @@
 import WriteBuffer from './write-buffer.js';
 import FileType from './file-types.js';
 import SGProp from './sgprop.js';
+import { kFileType, kFileTypeArray } from './symbols.js';
+import type Stream from './stream.js';
 
 // # Building()
 // Represents a single building from the building file.
 export default class Building {
-
-	static [Symbol.for('sc4.type')] = FileType.Building;
-	static [Symbol.for('sc4.type.array')] = true;
-
-	// ## constructor(opts)
-	constructor(opts) {
-		this.crc = 0x00000000;
-		this.mem = 0x00000000;
-		this.major = 0x0002;
-		this.minor = 0x0004;
-		this.zotWord = 0x0;
-		this.unknown1 = 0x00;
-		this.appearance = 0b00000101;
-		this.xMinTract = 0x00;
-		this.zMinTract = 0x00;
-		this.xMaxTract = 0x00;
-		this.zMaxTract = 0x00;
-		this.xTractSize = 0x0002;
-		this.zTractSize = 0x0002;
-		this.sgprops = [];
-		this.unknown2 = 0x00;
-		this.IID1 = this.IID = this.TID = this.GID = 0x00000000;
-		this.minZ = this.minY = this.minX = 0;
-		this.maxZ = this.maxY = this.maxX = 0;
-		this.orientation = 0x00;
-		this.scaffold = 0x01;
-		Object.assign(this, opts);
-	}
+	static [kFileType] = FileType.Building;
+	static [kFileTypeArray] = true;
+	crc = 0x00000000;
+	mem = 0x00000000;
+	major = 0x0002;
+	minor = 0x0004;
+	zotWord = 0x0;
+	unknown1 = 0x00;
+	appearance = 0b00000101;
+	xMinTract = 0x00;
+	zMinTract = 0x00;
+	xMaxTract = 0x00;
+	zMaxTract = 0x00;
+	xTractSize = 0x0002;
+	zTractSize = 0x0002;
+	sgprops: SGProp[] = [];
+	unknown2 = 0x00;
+	GID = 0x00000000;
+	TID = 0x00000000;
+	IID = 0x00000000;
+	IID1 = 0x00000000;
+	minX = 0;
+	minY = 0;
+	minZ = 0;
+	maxX = 0;
+	maxY = 0;
+	maxZ = 0;
+	orientation = 0x00;
+	scaffold = 0x01;
 
 	// ## move(dx, dy, dz)
 	// The move vector of the building contains [dx, dy, dz] and should be 
 	// given in meters! This is because apparently the min/max values of the 
 	// building are given in meters as well.
-	move(dx, dy, dz) {
+	move(dx: number | [number, number, number], dy: number, dz: number) {
 		if (Array.isArray(dx)) {
 			[dx, dy, dz] = dx;
 		}
-		dx = dx || 0;
-		dy = dy || 0;
-		dz = dz || 0;
+		dx = dx ?? 0;
+		dy = dy ?? 0;
+		dz = dz ?? 0;
 		this.minX += dx;
 		this.maxX += dx;
 		this.minY += dy;
@@ -58,15 +61,13 @@ export default class Building {
 		this.xMaxTract = 0x40 + Math.floor(this.maxX / 64);
 		this.zMinTract = 0x40 + Math.floor(this.minZ / 64);
 		this.zMaxTract = 0x40 + Math.floor(this.maxZ / 64);
-
-		// Return.
 		return this;
 
 	}
 
 	// ## parse(rs)
 	// Parses the building from a buffer wrapper up in a readable stream.
-	parse(rs) {
+	parse(rs: Stream) {
 		rs.size();
 		this.crc = rs.dword();
 		this.mem = rs.dword();
@@ -87,13 +88,7 @@ export default class Building {
 		this.zTractSize = rs.word();
 
 		// Read the SGProps.
-		const count = rs.dword();
-		const props = this.sgprops;
-		props.length = count;
-		for (let i = 0; i < count; i++) {
-			let prop = props[i] = new SGProp();
-			prop.parse(rs);
-		}
+		this.sgprops = rs.sgprops();
 
 		// There seems to be an error in the Wiki. The unknown byte should 
 		// come **after** the IID1, otherwise they're incorrect.
--- src/core/dbpf.d.ts
+++ src/core/dbpf.d.ts
@@ -1,7 +1,10 @@
+import type Header from "./dbpf-header.js";
+
 export default class DBPF {
 	buffer = Uint8Array | null;
 	file = string;
 	length: number;
+	header: Header;
 	readBytes(offset: number, length: number): Uint8Array;
 	readBytesAsync(offset: number, length: number): Promise<Uint8Array>;
 }
