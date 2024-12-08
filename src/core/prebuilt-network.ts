543cdbc48f666b5008a027425dcdd217f3d09231
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-08 09:19:37 +0100

    Migrate prebuilt network to TypeScript

--- src/core/prebuilt-network.ts
+++ src/core/prebuilt-network.ts
@@ -2,75 +2,83 @@
 import { FileType } from './enums.js';
 import Unknown from './unknown.js';
 import Vertex from './vertex.js';
+import type { byte, dword, float, word } from 'sc4/types';
+import type Stream from './stream.js';
+import { kFileType, kFileTypeArray } from './symbols.js';
+import type SGProp from './sgprop.js';
 
 // # PrebuiltNetwork
 // A class that is used for networks that use prebuilt models, such as 
 // elevated highways.
 export default class PrebuiltNetwork {
 
-	static [Symbol.for('sc4.type')] = FileType.PrebuiltNetwork;
-	static [Symbol.for('sc4.type.array')] = true;
+	static [kFileType] = FileType.PrebuiltNetwork;
+	static [kFileTypeArray] = true;
+	crc: dword = 0x00000000;
+	mem: dword = 0x00000000;
+	major: word = 0x0004;
+	minor: word = 0x0008;
+	zot: byte = 0x04;
+	appearance: byte = 0x05;
+	xMinTract: byte = 0x00;
+	zMinTract: byte = 0x00;
+	xMaxTract: byte = 0x00;
+	zMaxTract: byte = 0x00;
+	xTractSize: word = 0;
+	sgprops: SGProp[] = [];
+	GID: dword = 0x00000000;
+	TID: dword = 0x00000000;
+	IID: dword = 0x00000000;
+	x: float = 0;
+	y: float = 0;
+	z: float = 0;
+	vertices: [Vertex, Vertex, Vertex, Vertex] = [
+		new Vertex(),
+		new Vertex(),
+		new Vertex(),
+		new Vertex(),
+	];
+	modelId: dword = 0;
+	orientation: byte = 0x00;
+	networkType: byte = 0x02;
+	westConnection: byte = 0x00;
+	northConnection: byte = 0x00;
+	eastConnection: byte = 0x00;
+	southConnection: byte = 0x00;
+	xMin: float = 0;
+	xMax: float = 0;
+	yMin: float = 0;
+	yMax: float = 0;
+	zMin: float = 0;
+	zMax: float = 0;
+	rest: Uint8Array = new Uint8Array();
+	u = new Unknown();
 
 	// ## constructor()
+	// The constructor sets up some default unknown values.
 	constructor() {
-		let u = new Unknown(this);
-		this.crc = 0x00000000;
-		this.mem = 0x00000000;
-		this.major = 0x0004;
-		this.minor = 0x0008;
-		this.zot = 0x04;
+		const { u } = this;
 		u.dword(0x00000000);
-		this.appearance = 0x05;
 		u.dword(0xc772bf98);
-		this.xMinTract = 0x00;
-		this.zMinTract = 0x00;
-		this.xMaxTract = 0x00;
-		this.zMaxTract = 0x00;
-		this.sgprops = [];
-		this.GID = 0x00000000;
-		this.TID = 0x00000000;
-		this.IID = 0x00000000;
 		u.byte(0x01);
-		this.x = 0;
-		this.y = 0;
-		this.z = 0;
-		this.vertices = [
-			new Vertex(),
-			new Vertex(),
-			new Vertex(),
-			new Vertex(),
-		];
 		u.bytes([0, 0, 0, 0, 0]);
-		this.orientation = 0x00;
-		this.networkType = 0x02;
-		this.westConnection = 0x00;
-		this.northConnection = 0x00;
-		this.eastConnection = 0x00;
-		this.southConnection = 0x00;
 		u.bytes([0, 0, 0, 0, 0, 0, 0]);
-		this.xMin = 0;
-		this.xMax = 0;
-		this.yMin = 0;
-		this.yMax = 0;
-		this.zMin = 0;
-		this.zMax = 0;
 		u.bytes([0, 0, 0, 0]);
 		repeat(4, () => u.dword(0x00000000));
-		this.rest = [];
 	}
 
-	// ## parse(rs)
-	parse(rs) {
-		let u = new Unknown(this);
+	parse(rs: Stream): this {
+		this.u = new Unknown();
+		const unknown = this.u.reader(rs);
 		rs.size();
 		this.crc = rs.dword();
 		this.mem = rs.dword();
 		this.major = rs.word();
 		this.minor = rs.word();
 		this.zot = rs.byte();
-		u.dword(rs.dword());
+		unknown.dword();
 		this.appearance = rs.byte();
-		u.dword(rs.dword());
+		unknown.dword();
 		this.xMinTract = rs.byte();
 		this.zMinTract = rs.byte();
 		this.xMaxTract = rs.byte();
@@ -81,7 +89,7 @@ export default class PrebuiltNetwork {
 		this.GID = rs.dword();
 		this.TID = rs.dword();
 		this.IID = rs.dword();
-		u.byte(rs.byte());
+		unknown.byte();
 		this.x = rs.float();
 		this.y = rs.float();
 		this.z = rs.float();
@@ -92,29 +100,30 @@ export default class PrebuiltNetwork {
 			rs.vertex(),
 		];
 		this.modelId = rs.dword();
-		u.bytes(rs.read(5));
+		unknown.bytes(5);
 		this.orientation = rs.byte();
 		this.networkType = rs.byte();
 		this.westConnection = rs.byte();
 		this.northConnection = rs.byte();
 		this.eastConnection = rs.byte();
 		this.southConnection = rs.byte();
-		u.bytes(rs.read(7));
+		unknown.bytes(7);
 		this.xMin = rs.float();
 		this.xMax = rs.float();
 		this.yMin = rs.float();
 		this.yMax = rs.float();
 		this.zMin = rs.float();
 		this.zMax = rs.float();
-		u.bytes(rs.read(4));
-		repeat(4, () => u.dword(rs.dword()));
+		unknown.bytes(4);
+		repeat(4, () => unknown.dword());
 		this.rest = rs.rest();
 		rs.assert();
+		return this;
 	}
 
 }
 
-function repeat(n, fn) {
+function repeat(n: number, fn: () => void): void {
 	for (let i = 0; i < n; i++) {
 		fn();
 	}
