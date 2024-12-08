fd2746a8bd8e99d05b6196ea72714e3dece86614
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-08 09:15:48 +0100

    Update unknown.ts

--- src/core/unknown.ts
+++ src/core/unknown.ts
@@ -1,37 +1,37 @@
 // # unknown.js
 import type { byte, double, dword, float, qword, word } from 'sc4/types';
+import type Stream from './stream.js';
+import type WriteBuffer from './write-buffer.js';
 type UnknownType = number | boolean | bigint | Uint8Array;
 
 // # Unknown
 // Helper class that we use for easily managing unknown values in data 
 // structures used by the game.
 export default class Unknown extends Array<UnknownType> {
+	bool(value: boolean ) { this.push(value); }
+	byte(value: byte) { this.push(value); }
+	word(value: word) { this.push(value); }
+	dword(value: dword) { this.push(value); }
+	qword(value: qword) { this.push(value); }
+	float(value: float) { this.push(value); }
+	double(value: double) { this.push(value); }
+	bytes(value: number[] | Uint8Array) {
+		if (Array.isArray(value)) {
+			this.push(new Uint8Array(value));
+		} else {
+			this.push(value);
+		}
+	}
 
-	constructor(struct: object) {
-		super();
-		Object.defineProperty(this, 'struct', {
-			enumerable: false,
-			writable: false,
-			value: struct,
-		});
-		Object.defineProperty(struct, 'unknown', {
-			enumerable: false,
-			writable: true,
-			configurable: true,
-			value: this,
-		});
+	// ## reader(rs)
+	reader(rs: Stream) {
+		return new UnknownReader(this, rs);
 	}
-	add(item: UnknownType) {
-		this.push(item);
+
+	// ## writer(ws)
+	writer(ws: WriteBuffer) {
+		return new UnknownWriter(this, ws);
 	}
-	bool(value: boolean) { this.add(value); }
-	byte(value: byte) { this.add(value); }
-	word(value: word) { this.add(value); }
-	dword(value: dword) { this.add(value); }
-	qword(value: qword) { this.add(value); }
-	float(value: float) { this.add(value); }
-	double(value: double) { this.add(value); }
-	bytes(value: Uint8Array) { this.add(new Uint8Array(value)); }
 
 	// ## generator()
 	// Creates a generator function that consumes the unknown one by one. This 
@@ -54,6 +54,7 @@ export default class Unknown extends Array<UnknownType> {
 			byte: () => fn() as byte,
 			word: () => fn() as word,
 			dword: () => fn() as dword,
+			qword: () => fn() as qword,
 			float: () => fn() as float,
 			double: () => fn() as double,
 			bytes: () => fn() as Uint8Array,
@@ -65,4 +66,42 @@ export default class Unknown extends Array<UnknownType> {
 		});
 	}
 
+}
+
+// # UnknownReader
+// A helper class to be used when reading unknowns.
+class UnknownReader {
+	unknown: Unknown;
+	rs: Stream;
+	constructor(unknown: Unknown, rs: Stream) {
+		this.unknown = unknown;
+		this.rs = rs;
+	}
+	bool() { this.unknown.bool(this.rs.bool()); }
+	byte() { this.unknown.byte(this.rs.byte()); }
+	word() { this.unknown.word(this.rs.word()); }
+	dword() { this.unknown.dword(this.rs.dword()); }
+	qword() { this.unknown.qword(this.rs.qword()); }
+	float() { this.unknown.float(this.rs.float()); }
+	double() { this.unknown.double(this.rs.double()); }
+	bytes(length?: number) { this.unknown.bytes(this.rs.read(length)); }
+}
+
+// # UnkownWriter
+class UnknownWriter {
+	ws: WriteBuffer;
+	generator: ReturnType<Unknown['generator']>;
+	constructor(unknown: Unknown, ws: WriteBuffer) {
+		this.generator = unknown.generator();
+		this.ws = ws;
+	}
+	bool() { this.ws.bool(this.generator.bool()) };
+	byte() { this.ws.byte(this.generator.byte()) };
+	word() { this.ws.word(this.generator.word()) };
+	dword() { this.ws.dword(this.generator.dword()) };
+	qword() { this.ws.qword(this.generator.qword()) };
+	float() { this.ws.float(this.generator.float()) };
+	double() { this.ws.double(this.generator.double()) };
+	bytes() { this.ws.write(this.generator.bytes()) };
+	assert() { this.generator.assert(); }
 }
