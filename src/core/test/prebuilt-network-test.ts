3abf77b7163f2fd43b0a9350caa73925c3542621
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-17 15:48:46 +0100

    Add tests

--- src/core/test/prebuilt-network-test.ts
+++ src/core/test/prebuilt-network-test.ts
@@ -2,6 +2,7 @@
 import { expect } from 'chai';
 import { resource } from '#test/files.js';
 import { FileType, Savegame } from 'sc4/core';
+import { compareUint8Arrays } from 'uint8array-extras';
 
 describe('The prebuilt network file', function() {
 
@@ -15,6 +16,12 @@ describe('The prebuilt network file', function() {
 			let buff = Buffer.from(tile.toBuffer());
 			expect(buff.readUInt32LE(4)).to.equal(crc);
 		}
+
+		// Serialize the entire file right away. Should result in exactly the 
+		// same buffer.
+		let source = entry.decompress();
+		let check = entry.toBuffer()!;
+		expect(compareUint8Arrays(source, check)).to.equal(0);
 
 	});
 
