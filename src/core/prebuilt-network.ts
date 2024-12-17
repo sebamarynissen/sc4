8996c1fc117231a6f6fbaf09d17659fc239a3e17
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-17 15:19:54 +0100

    Update prebuilt network structure
    Apparently the prebuilt network structure has been further decoded in the meantime, so we've updated it.

--- src/core/prebuilt-network.ts
+++ src/core/prebuilt-network.ts
@@ -2,13 +2,15 @@
 import { FileType } from './enums.js';
 import Unknown from './unknown.js';
 import Vertex from './vertex.js';
-import type { byte, dword, word } from 'sc4/types';
+import type { byte, dword, float, qword, TGIArray, word } from 'sc4/types';
 import type Stream from './stream.js';
 import { kFileType, kFileTypeArray } from './symbols.js';
 import type SGProp from './sgprop.js';
 import Box3 from './box-3.js';
 import TractInfo from './tract-info.js';
 import Vector3 from './vector-3.js';
+import type Matrix3 from './matrix-3.js';
+import NetworkCrossing from './network-crossing.js';
 
 // # PrebuiltNetwork
 // A class that is used for networks that use prebuilt models, such as 
@@ -18,16 +20,13 @@ export default class PrebuiltNetwork {
 	static [kFileTypeArray] = true;
 	crc: dword = 0x00000000;
 	mem: dword = 0x00000000;
-	major: word = 0x0004;
-	minor: word = 0x0008;
-	zot: byte = 0x04;
-	appearance: byte = 0x05;
+	version = '4.8.4';
+	appearance: dword = 0x05;
 	tract = new TractInfo();
 	xTractSize: word = 0;
 	sgprops: SGProp[] = [];
-	GID: dword = 0x00000000;
-	TID: dword = 0x00000000;
-	IID: dword = 0x00000000;
+	tgi: TGIArray = [0x00000000, 0x00000000, 0x00000000];
+	matrix3: Matrix3 | null = null;
 	position = new Vector3();
 	vertices: [Vertex, Vertex, Vertex, Vertex] = [
 		new Vertex(),
@@ -36,28 +35,31 @@ export default class PrebuiltNetwork {
 		new Vertex(),
 	];
 	modelId: dword = 0;
+	wealthTexture: byte = 0x00;
+	baseTexture: dword = 0x00000000;
 	orientation: byte = 0x00;
+	crossings: NetworkCrossing[] = [];
 	networkType: byte = 0x02;
 	westConnection: byte = 0x00;
 	northConnection: byte = 0x00;
 	eastConnection: byte = 0x00;
 	southConnection: byte = 0x00;
+	walls: ({ texture: dword, vertex: Vertex })[] = [];
 	bbox = new Box3();
-	rest: Uint8Array = new Uint8Array();
-	u = new Unknown();
-
-	// ## constructor()
-	// The constructor sets up some default unknown values.
-	constructor() {
-		const { u } = this;
-		u.dword(0x00000000);
-		u.dword(0xc772bf98);
-		u.byte(0x01);
-		u.bytes([0, 0, 0, 0, 0]);
-		u.bytes([0, 0, 0, 0, 0, 0, 0]);
-		u.bytes([0, 0, 0, 0]);
-		repeat(4, () => u.dword(0x00000000));
-	}
+	constructionStates: dword = 0x00000000;
+	pathId: dword = 0x00000000;
+	demolishingCosts: qword = 0n;
+	pillar: { id: dword; rotation: float; position: Vector3 } | null = null;
+	u = new Unknown()
+		.dword(0xc772bf98)
+		.bytes([0, 0])
+		.dword(0x00000000)
+		.dword(0x00000000)
+		.dword(0x00000000)
+		.float(0)
+		.float(0)
+		.float(0)
+		.bytes([0, 0, 0, 0, 0]);
 
 	parse(rs: Stream): this {
 		this.u = new Unknown();
@@ -65,18 +67,14 @@ export default class PrebuiltNetwork {
 		rs.size();
 		this.crc = rs.dword();
 		this.mem = rs.dword();
-		this.major = rs.word();
-		this.minor = rs.word();
-		this.zot = rs.byte();
-		unknown.dword();
-		this.appearance = rs.byte();
-		unknown.dword();
+		this.version = rs.version(3);
+		this.appearance = rs.dword();
+		unknown.dword(0xc772bf98);
 		this.tract = rs.tract();
 		this.sgprops = rs.sgprops();
-		this.GID = rs.dword();
-		this.TID = rs.dword();
-		this.IID = rs.dword();
-		unknown.byte();
+		this.tgi = rs.tgi();
+		let hasMatrix = rs.byte() === 0x05;
+		this.matrix3 = hasMatrix ? rs.matrix3() : null;
 		this.position = rs.vector3();
 		this.vertices = [
 			rs.vertex(),
@@ -85,26 +83,41 @@ export default class PrebuiltNetwork {
 			rs.vertex(),
 		];
 		this.modelId = rs.dword();
-		unknown.bytes(5);
+		this.wealthTexture = rs.byte();
+		this.baseTexture = rs.dword();
 		this.orientation = rs.byte();
-		this.networkType = rs.byte();
-		this.westConnection = rs.byte();
-		this.northConnection = rs.byte();
-		this.eastConnection = rs.byte();
-		this.southConnection = rs.byte();
-		unknown.bytes(7);
+		unknown.bytes(2);
+		this.crossings = new Array<NetworkCrossing>(rs.byte()+1);
+		for (let i = 0; i < this.crossings.length; i++) {
+			let crossing = new NetworkCrossing().parse(rs);
+			this.crossings[i] = crossing;
+		}
+		this.walls = rs.array(() => {
+			let texture = rs.dword();
+			let vertex = rs.vertex();
+			return { texture, vertex };
+		});
 		this.bbox = rs.bbox({ range: true });
-		unknown.bytes(4);
-		repeat(4, () => unknown.dword());
-		this.rest = rs.rest();
+		this.constructionStates = rs.dword();
+		this.pathId = rs.dword();
+		unknown.dword(0x00000000);
+		unknown.dword(0x00000000);
+		unknown.dword(0x00000000);
+		this.demolishingCosts = rs.qword();
+		unknown.float();
+		unknown.float();
+		unknown.float();
+		unknown.bytes(5);
+		let pillar = rs.dword();
+		if (pillar > 0) {
+			this.pillar = {
+				id: pillar,
+				rotation: rs.float(),
+				position: rs.vector3(),
+			};
+		}
 		rs.assert();
 		return this;
 	}
 
-}
-
-function repeat(n: number, fn: () => void): void {
-	for (let i = 0; i < n; i++) {
-		fn();
-	}
 }
