e5694fbb3c4abb2095a06429999f23cef4cf467e
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-06 17:33:48 +0100

    Finalize test for unpacking submenu

--- src/plugins/test/unpack-submenu-test.ts
+++ src/plugins/test/unpack-submenu-test.ts
@@ -3,10 +3,12 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { resource, output } from '#test/files.js';
 import unpackSubmenu from '../unpack-submenu.js';
+import { expect } from 'chai';
+import { parse } from 'yaml';
 
 describe('#unpackSubmenu()', function() {
 
-	it.only('unpacks a submenu button', async function() {
+	it('unpacks a submenu button', async function() {
 
 		let directory = resource('unpack_submenu_test');
 		let dist = output('unpack_submenu_test');
@@ -20,6 +22,30 @@ describe('#unpackSubmenu()', function() {
 			directory,
 			output: dist,
 		});
+
+		const exists = (x: string) => fs.existsSync(path.join(dist, x));
+		const read = (x: string) => String(fs.readFileSync(path.join(dist, x)));
+		const yaml = (x: string) => parse(read(x));
+
+		expect(exists('orphans')).to.be.false;
+		expect(yaml('0x00000000-airport/_0xFFFFFE80-submenu-modular-airport/_menu.yaml')).to.eql({
+			id: 0xc1e9138f,
+			name: 'Modular Airport Lots',
+			description: [ 0x2026960b, 0x123006aa, 0x6e967dff ],
+		});
+		expect(yaml('0x00000000-airport/_0xFFFFFE80-submenu-modular-airport/_0xFFFFFF01-submenu-atc/_menu.yaml')).to.eql({
+			id: 0x803da728,
+			name: 'Air traffic control systems',
+			description: [ 0x2026960b, 0x123006aa, 0x6e967dff ],
+		});
+		expect(yaml('0x00000000-airport/_0xFFFFFE80-submenu-modular-airport/_0xFFFFFF01-submenu-atc/_0xFFFFFF02-submenu-atc-towers/_menu.yaml')).to.eql({
+			id: 0x414efa54,
+			name: 'ATC towers',
+			description: [ 0x2026960b, 0x123006aa, 0x6e967dff ],
+		});
+
+		let patch = read('0x00000000-airport/_0xFFFFFE80-submenu-modular-airport/_0xFFFFFF01-submenu-atc/_0xFFFFFF02-submenu-atc-towers/Patch ATC Towers.txt');
+		expect(patch).to.be.ok;
 
 	});
 
