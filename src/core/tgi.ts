a778f5ef47c385c33c28176bc3dfb1276b4ef811
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-08 17:04:02 +0100

    Add TGI class

---
+++ src/core/tgi.ts
@@ -0,0 +1,47 @@
+// # tgi.ts
+import type { TGIArray, TGILiteral, uint32 } from 'sc4/types'
+import { randomId, inspect } from 'sc4/utils';
+
+export default class TGI<T extends uint32 = uint32> {
+	type: T = 0x00000000 as T;
+	group: uint32 = 0x00000000;
+	instance: uint32 = 0x00000000;
+
+	// Generates a random tgi, useful for testing.
+	static random<T extends number = uint32>(
+		type: T = randomId() as T,
+		group: uint32 = randomId(),
+		instance: uint32 = randomId(),
+	) {
+		return new TGI(type, group, instance);
+	}
+
+	constructor(tgi: TGIArray<T> | TGILiteral<T>);
+	constructor(type: T, group: uint32, instance: uint32);
+	constructor(tgiOrType: TGIArray<T> | TGILiteral<T> | T, group?: uint32, instance?: uint32) {
+		let type;
+		if (Array.isArray(tgiOrType)) {
+			[type, group, instance] = tgiOrType;
+		} else if (typeof tgiOrType === 'number') {
+			type = tgiOrType;
+		} else {
+			({ type, group, instance } = tgiOrType);
+		}
+		this.type = type;
+		this.group = group!;
+		this.instance = instance!;
+	}
+
+	[Symbol.for('nodejs.util.inspect.custom')](_level: any, opts: any, nodeInspect: any) {
+		let str = [...this]
+			.map(nr => nodeInspect(inspect.hex(nr), opts))
+			.join(', ');
+		return `TGI(${str})`;
+	}
+
+	*[Symbol.iterator]() {
+		yield this.type;
+		yield this.group;
+		yield this.instance;
+	}
+}
