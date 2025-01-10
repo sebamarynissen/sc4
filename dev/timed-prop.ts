27c8419eff5bab35d5a6a97fd201f4ecdfe466ac
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-10 16:58:15 +0100

    Try to properly decode prop developr

---
+++ dev/timed-prop.ts
@@ -0,0 +1,51 @@
+// # timed-prop.ts
+import { resource } from "#test/files.js";
+import CityManager from "sc4/api/city-manager.js";
+import { FileType, Savegame } from "sc4/core";
+import { PluginIndex } from "sc4/plugins";
+import path from 'node:path';
+import { removePointers } from "src/core/helpers.js";
+
+run(new Savegame(resource('City - CP before.sc4')));
+run(new Savegame(resource('City - CP after.sc4')));
+run(new Savegame(resource('City - Conditional.sc4')));
+run(new Savegame(resource('City - RCI.sc4')));
+run(new Savegame(resource('City - Large developed.sc4')));
+run(new Savegame(resource('city.sc4')));
+run(new Savegame(path.join(process.env.SC4_REGIONS!, 'New Sebastia - 2016-07-25 15.15/City - Sebastia (2).sc4')));
+run(new Savegame(path.join(process.env.SC4_REGIONS!, 'New Sebastia - 2016-07-25 15.15/City - Maybeline.sc4')));
+
+function run(dbpf: Savegame) {
+
+	let entry = dbpf.find({ type: FileType.PropDeveloper })!;
+	// let buffer = entry.decompress();
+
+	entry.read();
+
+	// removePointers(buffer);
+	// buffer.set([0, 0, 0, 0, 0, 0, 0, 0], 4);
+	// console.log(`new Uint8Array([${buffer}])`);
+
+}
+
+// let output = path.resolve(process.env.SC4_REGIONS!, 'suburb/City - proplop.sc4');
+// let source = new Savegame(resource('City - proplop.sc4'));
+// // let source = new Savegame(output);
+// let index = new PluginIndex({
+// 	core: false,
+// });
+// await index.build();
+// await index.buildFamilies();
+// let mgr = new CityManager({
+// 	dbpf: source,
+// 	index,
+// });
+
+// mgr.plop({
+// 	tgi: [0x6534284a, 0x07bddf1c, 0x550480c4],
+// 	x: 0,
+// 	z: 0,
+// 	orientation: 2,
+// });
+
+// source.save(output);
--- src/core/prop-developer.ts
+++ src/core/prop-developer.ts
@@ -2,9 +2,37 @@ import type { dword, word } from 'sc4/types';
 import FileType from './file-types.js';
 import { kFileType } from './symbols.js';
 import Unknown from './unknown.js';
-import type Stream from './stream.js';
+import Stream from './stream.js';
 import type Pointer from './pointer.js';
 import WriteBuffer from './write-buffer.js';
+import { SmartBuffer } from 'smart-arraybuffer';
+import { cClass } from './enums.js';
+
+function readBackwards(buffer: Uint8Array) {
+
+	// As long as we encounter pointers, keep on going.
+	let count = 0;
+	let offset = buffer.byteLength-4;
+	let reader = SmartBuffer.fromBuffer(buffer);
+	let pointer = reader.readUInt32LE(offset);
+	while (pointer !== 0 && String(pointer) in cClass) {
+		offset -= 8;
+		count++;
+		pointer = reader.readUInt32LE(offset);
+	}
+	if (reader.readUInt32LE(offset) !== count) {
+		throw new Error('This is not a pointer array');
+	}
+	let slice = buffer.subarray(0, offset);
+	let rs = new Stream(buffer.subarray(offset));
+	let pointers = rs.array(() => rs.pointer()!);
+	return { slice, pointers };
+
+}
+
+function format(buffer: Uint8Array) {
+	return [...buffer].map(x => x.toString(16).padStart(2, '0')).join(' ');
+}
 
 // # prop-developer.ts
 export default class PropDeveloper {
@@ -19,8 +47,9 @@ export default class PropDeveloper {
 	propManager: Pointer;
 	networkLotManager: Pointer;
 	count1: number = 0;
-	array1: Pointer[];
 	adviceArray: dword[];
+	array1: Pointer[];
+	array2: Pointer[];
 	array3: Pointer[];
 	array4: Pointer[];
 	array5: Pointer[];
@@ -60,21 +89,43 @@ export default class PropDeveloper {
 		this.propManager = rs.pointer()!;
 		this.networkLotManager = rs.pointer()!;
 		this.count1 = rs.dword();
-		if (this.count1 > 0) {
-			unknown.dword();
-		}
-		unknown.dword();
-		unknown.dword();
-		unknown.byte();
-		this.array1 = rs.array(() => rs.pointer()!);
-		this.adviceArray = rs.array(() => rs.dword());
-		this.array3 = rs.array(() => rs.pointer()!);
-		this.array4 = rs.array(() => rs.pointer()!);
-		this.array5 = rs.array(() => rs.pointer()!);
-		this.array6 = rs.array(() => rs.pointer()!);
-		unknown.dword();
-		unknown.byte();
-		rs.assert();
+		let header = rs.read(17);
+
+		// Next we'll do something fancy. We don't really know the structure 
+		// yet, of what follows, but we *do* know the structure at the end. 
+		// Hence we'll consume the buffer *from the back*.
+		let slice = rs.read(rs.remaining()-5);
+		({ slice } = readBackwards(slice));
+		({ slice } = readBackwards(slice));
+		({ slice } = readBackwards(slice));
+		({ slice } = readBackwards(slice));
+		let rest = slice;
+
+		// unknown.dword();
+		// unknown.dword();
+		// unknown.byte();
+		// this.array1 = rs.array(() => rs.pointer()!);
+		// if (this.count1 > 0) {
+		// 	this.array2 = rs.array(() => rs.pointer()!);
+		// } else {
+		// 	this.array2 = [];
+		// }
+		// rs.array(() => rs.dword());
+		// this.array3 = rs.array(() => rs.pointer()!);
+		// this.array4 = rs.array(() => rs.pointer()!);
+		// this.array5 = rs.array(() => rs.pointer()!);
+		// this.array6 = rs.array(() => rs.pointer()!);
+		// unknown.dword();
+		// unknown.byte();
+		// rs.assert();
+		// let rest = rs.read();
+		let postfix = rest.length > 64 ? '...' : '   ';
+		console.log(
+			String(this.count1).padEnd(8, ' '),
+			format(header),
+			postfix,
+			format(rest.subarray(-64)),
+		);
 		return this;
 	}
 	toBuffer() {
