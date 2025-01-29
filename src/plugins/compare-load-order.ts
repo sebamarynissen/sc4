a04163af6f1f391479f77a453f31b9ac84f8ce28
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-29 15:44:06 +0100

    Implement load order sort algorithm

---
+++ src/plugins/compare-load-order.ts
@@ -0,0 +1,38 @@
+// # sort-load-order.js
+// # createComparator()
+export default function createComparator() {
+	const table = new HashTable();
+	return function compare(a: string, b: string): number {
+		let ha = table.getHash(a);
+		let hb = table.getHash(b);
+		let min = Math.min(ha.length, hb.length)-1;
+		let i = 0;
+		for (; i < min; i++) {
+			let a = ha[i];
+			let b = hb[i];
+			if (a === b) continue;
+			return a < b ? -1 : 1;
+		}
+		
+		// If we haven't made a decision by now, it means either both files are 
+		// in the same folder, or one folder is a subfolder of the other one.
+		if (ha.length === hb.length) {
+			let da = ha[i].endsWith('.DAT') ? 1 : -1;
+			let db = hb[i].endsWith('.DAT') ? 1 : -1;
+			return da-db || ha[i] < hb[i] ? -1 : 1;
+		} else {
+			return ha.length < hb.length ? -1 : 1;
+		}
+
+	};
+}
+
+class HashTable {
+	table: Record<string, string[]> = Object.create(null);
+	getHash(file: string) {
+		let arr = this.table[file];
+		if (arr) return arr;
+		let parts = file.toUpperCase().split(/[\/\\]/);
+		return arr = this.table[file] = parts;
+	}
+}
