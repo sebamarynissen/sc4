77014b04833051dbc47a8d39507a640b00b445dd
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-04-10 17:58:29 +0200

    Add PoC for overriding version info

--- build/inject-icon.ts
+++ build/inject-icon.ts
@@ -1,9 +1,7 @@
 // # inject-icon.ts
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import cp from 'node:child_process';
-
 const exe = process.env.RESOURCE_HACKER_EXECUTABLE!;
-const source = path.join(process.cwd(), 'dist/sc4.exe');
-const icon = fileURLToPath(new URL(import.meta.resolve('./sc4.ico')));
-cp.execSync(`"${exe}" -open "${source}" -save "${source}" -action addoverwrite -res "${icon}" -mask ICONGROUP,MAINICON`);
+cp.execSync(`"${exe}" -open dist/sc4.exe -save dist/sc4-no-icon.exe -action delete -res 1033 -mask ICONGROUP`);
+cp.execSync(`"${exe}" -open dist/sc4-no-icon.exe -save dist/sc4-with-icon.exe -action addoverwrite -res build/sc4.ico -mask ICONGROUP,MAINICON,1033`);
+cp.execSync(`"${exe}" -open build/sc4.rc -save dist/sc4.res -action compile`);
+cp.execSync(`"${exe}" -open dist/sc4-with-icon.exe -save dist/sc4.exe -action addoverwrite -res dist/sc4.res -mask VERSIONINFO,1`);
